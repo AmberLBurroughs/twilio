a9b35c1c7acf39187457f3d8c1db1bcba75f4697
Amber Burroughs <amberlburroughs@gmail.com>
2018-12-29 01:56:24 -0800

    return 200 status after voicemail created

--- routes/voice.js
+++ routes/voice.js
@@ -76,7 +76,7 @@ router.post('/gather', (request, response) => {
         // });
         // twiml.say('I did not receive a recording');
         twiml.hangup();
-        // twiml.status(200)
+        twiml.status(200)
       break;
       case '3':
         return getSMSSchedule(callFrom);
