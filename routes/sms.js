b86de18656b2d72e3b00cffd278564cb31965dbb
Amber Burroughs <amberlburroughs@gmail.com>
2018-12-28 22:29:11 -0800

    change voice on call

--- routes/sms.js
+++ routes/sms.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 var express = require('express');
 var router = express.Router();
+// const keys          = require("../keys.js");
+// const accountSid    = keys.twilio.accountSid;
+// const authToken     = keys.twilio.authToken;
+// const client        = require('twilio')(accountSid, authToken);
 
 /* Programable SMS. */
 router.post('/', function(req, res, next) {
--- routes/voice.js
+++ routes/voice.js
@@ -1,22 +1,13 @@
-require("dotenv").config();
-const express = require('express');
-const router  = express.Router();
-
-const keys          = require("../keys.js");
+const express 			= require('express');
+const router  			= express.Router();
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
-// const accountSid    = keys.twilio.accountSid;
-// const authToken     = keys.twilio.authToken;
-// const client        = require('twilio')(accountSid, authToken);
-
 
 router.post('/', (request, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  console.log("caller data", request.body);
+
+  const callFrom = request.body.From;
 
   const twiml = new VoiceResponse();
-  twiml.say({ voice: 'Amber' }, 'hello world!');
-
-  // Render the response as XML in reply to the webhook request
+  twiml.say({ voice: 'alice', }, 'hello world!');
   response.type('text/xml');
   response.send(twiml.toString());
 });
